910e584fa279e479b99e6c27fd724d4f8cf8c137
whdgur5717 <whdgur57117@gmail.com>
2025-02-16 16:10:09 +0900

    chore(eslint): enhance ESLint configuration with new plugins and rules

--- configs/eslint/base.js
+++ configs/eslint/base.js
@@ -1,33 +1,83 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import turboPlugin from "eslint-plugin-turbo"
+import prettier from "eslint-plugin-prettier/recommended"
+import globals from "globals"
 import tseslint from "typescript-eslint"
-import onlyWarn from "eslint-plugin-only-warn"
+import pluginJs from "@eslint/js"
+import _import from "eslint-plugin-import"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import storybook from "eslint-plugin-storybook"
 
-/**
- * A shared ESLint configuration for the repository.
- *
- * @type {import("eslint").Linter.Config}
- * */
 export const config = [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.strict,
+  {
+    ignores: ["node_modules/**", "dist/**", "fixture/**"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals["shared-node-browser"],
+        ...globals.es2015,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        parser: tseslint.parser,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...storybook.configs["flat/recommended"],
+  prettier,
   {
     plugins: {
-      turbo: turboPlugin,
+      "simple-import-sort": simpleImportSort,
     },
+  },
+  {
+    files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  {
     rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "error",
+      "simple-import-sort/imports": "error", // import exports 정렬
+      "simple-import-sort/exports": "error",
+      "react/react-in-jsx-scope": "off", // React를 import하지 않아도 사용 가능
+      "import/no-anonymous-default-export": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-parameter-properties": "off",
+      "@typescript-eslint/no-inferrable-types": "warn",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          format: ["camelCase", "UPPER_CASE", "PascalCase"],
+          selector: "variable",
+          leadingUnderscore: "allow",
+        },
+        { format: ["camelCase", "PascalCase"], selector: "function" },
+        { format: ["PascalCase"], selector: "interface" },
+        { format: ["PascalCase"], selector: "typeAlias" },
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { ignoreRestSiblings: true, caughtErrors: "none" },
+      ],
+      "prefer-object-has-own": "error",
     },
-  },
-  {
-    plugins: {
-      onlyWarn,
-    },
-  },
-  {
-    ignores: ["dist/**", "build/**", "node_modules/**"],
   },
 ]
--- configs/eslint/next.js
+++ configs/eslint/next.js
@@ -1,49 +1,13 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import tseslint from "typescript-eslint"
-import pluginReactHooks from "eslint-plugin-react-hooks"
-import pluginReact from "eslint-plugin-react"
-import globals from "globals"
-import pluginNext from "@next/eslint-plugin-next"
+import { FlatCompat } from "@eslint/eslintrc"
 import { config as baseConfig } from "./base.js"
 
-/**
- * A custom ESLint configuration for libraries that use Next.js.
- *
- * @type {import("eslint").Linter.Config}
- * */
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
 export const config = [
   ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  {
-    ...pluginReact.configs.flat.recommended,
-    languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-      },
-    },
-  },
-  {
-    plugins: {
-      "@next/next": pluginNext,
-    },
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      ...pluginNext.configs["core-web-vitals"].rules,
-    },
-  },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-    },
-  },
+  ...compat.config({
+    extends: ["next", "next/core-web-vitals", "next/typescript"],
+  }),
 ]
