e4630a414ef1f3cf8a43a99febe3afee58ce758b
whdgur5717 <whdgur57117@gmail.com>
2025-02-16 16:16:02 +0900

    chore: apply shared eslint config to all packages - change lint scripts

---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,14 @@
+import { config } from "@jongh/eslint/react-internal"
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...config,
+  {
+    rules: {
+      "react/display-name": "off",
+    },
+  },
+  {
+    ignores: ["styled-system/*", ".storybook/*", "postcss.config.*"],
+  },
+]
