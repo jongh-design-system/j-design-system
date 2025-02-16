dc44afa8a76292092732bc8c65968aa0a4c1d99f
whdgur5717 <whdgur57117@gmail.com>
2025-02-16 16:21:46 +0900

    chore: enhance lint-staged - check only staged file - separate command for docs(nextjs)

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 const MODULES = [
-  { name: "docs", parent: "app" },
+  // { name: "docs", parent: "app" },
   { name: "ui", parent: "packages" },
   { name: "cli", parent: "packages" },
   { name: "panda-animation", parent: "packages" },
@@ -8,15 +8,21 @@ const MODULES = [
 const typeCheckConfigs = MODULES.reduce(
   (prev, { name, parent }) => ({
     ...prev,
-    [`./${parent}/${name}/**/*.{ts,tsx}`]: () =>
-      `pnpm run --filter ${name} check-type`,
+    [`./${parent}/${name}/**/*.{ts,tsx}`]: (filename) => [
+      `pnpm --filter ${name} lint ${filename}`,
+      `pnpm --filter ${name} check-type`,
+    ],
   }),
   {},
 )
 
 module.exports = {
+  "*": (filename) => `echo ${filename}`,
   "*": "./check-uppercase.sh",
-  "*.{ts,tsx}": "eslint --cache",
   "*.{ts,tsx,css,md}": "prettier --write",
   ...typeCheckConfigs,
+  "./app/docs/**/*.{ts,tsx}": (filename) => [
+    `pnpm --filter docs lint --file ${filename}`,
+    `pnpm --filter docs check-type`,
+  ],
 }
