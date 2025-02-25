f1f4dc332723dfb9ffd31da5f160ef55fb8d76dc
whdgur5717 <whdgur57117@gmail.com>
2025-02-25 15:24:03 +0900

    chore(docs): initial config for using mdx

--- app/docs/next.config.ts
+++ app/docs/next.config.ts
@@ -1,9 +1,13 @@
+import createMDX from "@next/mdx"
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   trailingSlash: true,
+  pageExtensions: ["js", "md", "mdx", "ts", "tsx"],
 }
 
-export default nextConfig
+const withMdx = createMDX({})
+
+export default withMdx(nextConfig)
