49a509b7ce26dd64870b089a7e70bdb598b6d435
whdgur5717 <whdgur57117@gmail.com>
2025-02-26 22:17:40 +0900

    feat(docs): main page's main section

--- app/docs/src/app/page.tsx
+++ app/docs/src/app/page.tsx
@@ -1,3 +1,7 @@
+import { center } from "@styled-system/patterns"
+import Link from "next/link"
+
+import { Button } from "@/components/button"
 import { Header } from "@/components/header"
 
 export default function MainPage() {
@@ -11,6 +15,16 @@ export default function MainPage() {
   return (
     <>
       <Header logo={<span>Design System</span>} navItems={navItems} />
+      <main
+        className={center({
+          flexGrow: "1",
+          bg: "gray.800",
+        })}
+      >
+        <Link href="/docs/introduction">
+          <Button size="lg">시작하기</Button>
+        </Link>
+      </main>
     </>
   )
 }
