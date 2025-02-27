fce8190367d99d763383eb158e4ab3d9d6d34064
whdgur5717 <whdgur57117@gmail.com>
2025-02-27 18:24:22 +0900

    refactor(utils): simplify package manager command resolution - using package-manager-detector API

--- packages/cli/src/common/utils/packageManager.ts
+++ packages/cli/src/common/utils/packageManager.ts
@@ -1,19 +1,16 @@
-import { detect } from "package-manager-detector"
-
-export async function getPackageManagerRunner(cwd: string) {
+import { detect, resolveCommand } from "package-manager-detector"
+/**
+ * 사용하는 패키지 매니저를 감지하고, install 관련 올바른 command를 return
+ *
+ * ex) npm -> npm install, pnpm -> pnpm add
+ *
+ * www.npmjs.com/package/package-manager-detector
+ */
+export async function getPackageManagerCommand(cwd: string, args: string[]) {
   const pm = await detect({ cwd })
-  if (!pm) {
-    throw new Error("package manager not found")
+  const command = resolveCommand(pm?.agent || "npm", "add", args)
+  if (!command) {
+    return undefined
   }
-  if (pm.name === "pnpm") {
-    return "pnpm exec"
-  }
-  if (pm.name === "bun") {
-    return "bunx"
-  }
-  if (pm.name === "yarn") {
-    return "yarn dlx"
-  } else {
-    return "npx"
-  }
+  return command
 }
