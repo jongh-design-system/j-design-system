53cc07dfcdbf7ca77cea4a54695f020e7047b5bb
이종혁 <120625398+whdgur5717@users.noreply.github.com>
2025-02-27 19:38:09 +0900

    add command에서 패키지 다운 로직 제거 (#130)
    * refactor(add): remove dependencies installation logic
    
    * refactor(add): remove transformPreset logic
    
    * refactor(utils): simplify package manager command resolution
    - using package-manager-detector API
    
    * refactor(add): replace package install with message
    - remove unused logic
    
    * chore(add): remove unused await
    
    * docs(changeset): remove package installation in add command
    
    * feat(cd): add caching step for playwright
    
    ---------
    
    Co-authored-by: whdgur5717 <whdgur57117@gmail.com>

--- packages/cli/src/commands/add/index.ts
+++ packages/cli/src/commands/add/index.ts
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
-import { confirm, intro, outro, select } from "@clack/prompts"
+import { confirm, intro, outro } from "@clack/prompts"
 import chalk from "chalk"
 import { Command } from "commander"
-import { execa } from "execa"
 import fs from "fs-extra"
-import { detect } from "package-manager-detector"
 import path from "path"
 import { z, ZodError } from "zod"
 
@@ -16,9 +14,9 @@ import {
   loadTSConfig,
 } from "@/common/get-config"
 import { resolveImport, resolvePandaConfig } from "@/common/resolve"
-import { transformImports, transformPreset } from "@/common/transform"
+import { transformImports } from "@/common/transform"
 import { configSchema, registrySchema } from "@/common/types"
-import { getPackageManagerRunner } from "@/common/utils/packageManager"
+import { getPackageManagerCommand } from "@/common/utils/packageManager"
 
 const addSchema = z.object({
   components: z.array(z.string()).optional(),
@@ -50,7 +48,7 @@ export const addCommand = new Command()
         loadComponentConfig(options.cwd),
       )
       //2. tsconfig.json 파일을 읽어온다
-      const tsconfig = await loadTSConfig(options.cwd)
+      const tsconfig = loadTSConfig(options.cwd)
       //3. panda.config.* 파일을 읽어온다
       const pandaConfigPath = await getPandacssConfigPath(options.cwd)
 
@@ -140,21 +138,6 @@ export const addCommand = new Command()
               file.content,
               componentsJson,
             )
-            //TODO: file의 타입에 따라 변경하기
-            if (file.name === "recipe.ts") {
-              //현재 export하고있는 recipe 변수명은 componentList[index]+Recipe
-              //이걸 preset.ts에 넣어줘야 함 - 현재는 이 파일이 root에 있다고 가정
-              //이 파일의 alias는 component alias / 컴포넌트명 / recipe.ts
-              transformPreset(
-                path.join(options.cwd, "preset.ts"),
-                `${componentList[index]}`,
-                path.join(
-                  componentsJson.components,
-                  `${componentList[index]}`,
-                  "recipe",
-                ),
-              )
-            }
 
             if (file.type === "ui") {
               fs.outputFileSync(path.join(src, file.name), convertedContent)
@@ -167,31 +150,20 @@ export const addCommand = new Command()
             }
           })
 
-          const pm = await detect({ cwd: options.cwd })
+          const packageManagerCommand = await getPackageManagerCommand(
+            options.cwd,
+            registry.dependencies || [],
+          )
 
-          let pmName = pm ? pm.name : ""
+          const outroMsg = !packageManagerCommand
+            ? `Cannot find your package manager, install this dependencies: ${registry.dependencies?.join(" ")}`
+            : `Run this command in the terminal : ${packageManagerCommand.command} ${packageManagerCommand.args.join(" ")}`
 
-          if (!pmName) {
-            const selected = await select({
-              message: "cannot find package manager, select",
-              options: [
-                { value: "npm", label: "npm" },
-                { value: "pnpm", label: "pnpm" },
-                { value: "yarn", label: "yarn" },
-              ],
-            })
-            pmName = selected as string
-          }
-          if (registry.dependencies?.length) {
-            await execa(pmName, [
-              pmName === "npm" ? "install" : "add",
-              ...registry.dependencies,
-            ])
-          }
-          const runner = await getPackageManagerRunner(options.cwd)
-          const [name, ...cmd] = runner.split(" ")
-          execa(name, [...cmd, "panda", "codegen"])
-          outro(info(`${componentList[index]} completed successfully`))
+          outro(
+            info(
+              `${componentList[index]} completed successfully \n ${outroMsg}`,
+            ),
+          )
         } catch (e) {
           console.log(e)
         }
