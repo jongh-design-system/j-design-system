bac90f75a6a489635e9e94b68dc6a515d85e3c85
whdgur5717 <whdgur57117@gmail.com>
2025-03-08 22:44:57 +0900

    feat(docs-component): create SwitchCase component for conditional rendering

---
+++ app/docs/src/components/switchCase.tsx
@@ -0,0 +1,19 @@
+import type { ReactElement } from "react"
+
+interface Props<Case extends string | number> {
+  caseBy: Partial<Record<Case, ReactElement>>
+  value?: Case | null
+  defaultComponent?: ReactElement | null
+}
+
+export function SwitchCase<Case extends string | number>({
+  value,
+  caseBy,
+  defaultComponent: defaultComponent = null,
+}: Props<Case>) {
+  if (!value) {
+    return defaultComponent
+  }
+
+  return caseBy[value] ?? defaultComponent
+}
