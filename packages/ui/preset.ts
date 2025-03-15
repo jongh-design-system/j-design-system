1715232f806e3dd0df25430400ba558ee53e4f66
whdgur5717 <whdgur57117@gmail.com>
2025-03-15 15:52:34 +0900

    feat(ui): apply new preset

--- packages/ui/preset.ts
+++ packages/ui/preset.ts
@@ -1,101 +1,76 @@
-import {
-  defineGlobalStyles,
-  definePreset,
-  defineSemanticTokens,
-  defineTokens,
-} from "@pandacss/dev"
+import { definePreset, defineSemanticTokens } from "@pandacss/dev"
 
-const globalCss = defineGlobalStyles({
-  "html, body": {
-    w: "full",
-    h: "full",
-  },
-})
-
-const radii = defineTokens.radii({
-  radius: { value: "0.5rem" },
-})
-
-export const semanticColors = defineSemanticTokens.colors({
+const colors = defineSemanticTokens.colors({
   background: {
-    value: { base: "hsl(0 0% 100%)", _dark: "hsl(222.2 84% 4.9%)" },
+    value: { base: "{colors.white}", _dark: "{colors.gray.950}" },
   },
   foreground: {
-    value: { base: "hsl(222.2 84% 4.9%)", _dark: "hsl(210 40% 98%)" },
+    value: { base: "{colors.gray.950}", _dark: "{colors.gray.50}" },
   },
   card: {
     DEFAULT: {
-      value: { base: "hsl(0 0% 100%)", _dark: "hsl(222.2 84% 4.9%)" },
+      value: { base: "{colors.white}", _dark: "{colors.gray.900}" },
     },
     foreground: {
-      value: { base: "hsl(222.2 84% 4.9%)", _dark: "hsl(210 40% 98%)" },
+      value: { base: "{colors.gray.950}", _dark: "{colors.gray.50}" },
     },
   },
   popover: {
     DEFAULT: {
-      value: { base: "hsl(0 0% 100%)", _dark: "hsl(222.2 84% 4.9%)" },
+      value: { base: "{colors.white}", _dark: "{colors.gray.900}" },
     },
     foreground: {
-      value: { base: "hsl(222.2 84% 4.9%)", _dark: "hsl(210 40% 98%)" },
+      value: { base: "{colors.gray.950}", _dark: "{colors.gray.50}" },
     },
   },
   primary: {
     DEFAULT: {
-      value: { base: "hsl(222.2 47.4% 11.2%)", _dark: "hsl(210 40% 98%)" },
+      value: { base: "{colors.neutral.600}", _dark: "{colors.neutral.400}" },
     },
     foreground: {
-      value: { base: "hsl(210 40% 98%)", _dark: "hsl(222.2 47.4% 11.2%)" },
+      value: { base: "{colors.white}", _dark: "{colors.gray.950}" },
     },
   },
   secondary: {
     DEFAULT: {
-      value: { base: "hsl(210 40% 96.1%)", _dark: "hsl(217.2 32.6% 17.5%)" },
+      value: { base: "{colors.blue.600}", _dark: "{colors.blue.400}" },
     },
     foreground: {
-      value: { base: "hsl(222.2 47.4% 11.2%)", _dark: "hsl(210 40% 98%)" },
+      value: { base: "{colors.white}", _dark: "{colors.gray.950}" },
     },
   },
   muted: {
     DEFAULT: {
-      value: { base: "hsl(210 40% 96.1%)", _dark: "hsl(217.2 32.6% 17.5%)" },
+      value: { base: "{colors.gray.100}", _dark: "{colors.gray.800}" },
     },
     foreground: {
-      value: {
-        base: "hsl(215.4 16.3% 46.9%)",
-        _dark: "hsl(215 20.2% 65.1%)",
-      },
+      value: { base: "{colors.gray.500}", _dark: "{colors.gray.400}" },
     },
   },
   accent: {
     DEFAULT: {
-      value: { base: "hsl(210 40% 96.1%)", _dark: "hsl(217.2 32.6% 17.5%)" },
+      value: { base: "{colors.blue.100}", _dark: "{colors.blue.800}" },
     },
     foreground: {
-      value: { base: "hsl(222.2 47.4% 11.2%)", _dark: "hsl(210 40% 98%)" },
+      value: { base: "{colors.blue.900}", _dark: "{colors.blue.50}" },
     },
   },
   destructive: {
     DEFAULT: {
-      value: { base: "hsl(0 84.2% 60.2%)", _dark: "hsl(0 62.8% 30.6%)" },
+      value: { base: "{colors.red.500}", _dark: "{colors.red.400}" },
     },
     foreground: {
-      value: { base: "hsl(210 40% 98%)", _dark: "hsl(210 40% 98%)" },
+      value: { base: "{colors.white}", _dark: "{colors.gray.950}" },
     },
   },
   border: {
-    value: {
-      base: "hsl(214.3 31.8% 91.4%)",
-      _dark: "hsl(217.2 32.6% 17.5%)",
-    },
+    value: { base: "{colors.gray.200}", _dark: "{colors.gray.800}" },
   },
   input: {
-    value: {
-      base: "hsl(214.3 31.8% 91.4%)",
-      _dark: "hsl(217.2 32.6% 17.5%)",
-    },
+    value: { base: "{colors.gray.200}", _dark: "{colors.gray.800}" },
   },
   ring: {
-    value: { base: "hsl(222.2 84% 4.9%)", _dark: "hsl(212.7 26.8% 83.9%)" },
+    value: { base: "{colors.neutral.500}", _dark: "{colors.neutral.400}" },
   },
 })
 
@@ -106,29 +81,20 @@ const borders = defineSemanticTokens.borders({
   destructive: { value: "1px solid {colors.destructive}" },
 })
 
-export const radius = defineSemanticTokens.radii({
-  xl: { value: `calc({radii.radius} + 4px)` },
-  lg: { value: `{radii.radius}` },
-  md: { value: `calc({radii.radius} - 2px)` },
-  sm: { value: "calc({radii.radius} - 4px)" },
-})
-
 export const defaultPreset = definePreset({
   name: "default",
-  globalCss,
-  theme: {
-    extend: {
-      tokens: {
-        radii,
-      },
-      semanticTokens: {
-        colors: semanticColors,
-        radii: radius,
-        borders: borders,
-      },
+  globalCss: {
+    html: {
+      background: "background",
+      color: "foreground",
     },
   },
-  staticCss: {
-    recipes: "*",
+  theme: {
+    extend: {
+      semanticTokens: {
+        colors,
+        borders,
+      },
+    },
   },
 })
