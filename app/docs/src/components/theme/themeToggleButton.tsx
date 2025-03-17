9af3a3c537238c08df9486a272762c0484f43add
whdgur5717 <whdgur57117@gmail.com>
2025-03-17 21:36:06 +0900

    feat(theme): remove SwitchCase

--- app/docs/src/components/theme/themeToggleButton.tsx
+++ app/docs/src/components/theme/themeToggleButton.tsx
@@ -2,11 +2,8 @@
 import { motion } from "motion/react"
 import { useTheme } from "next-themes"
 
-import Darkmode from "@/components/icons/darkmode"
 import Lightmode from "@/components/icons/lightmode"
 import { useIsMounted } from "@/hooks/useIsMounted"
-
-import { SwitchCase } from "../switchCase"
 
 export const ThemeToggleButton = () => {
   const isMounted = useIsMounted()
@@ -18,42 +15,28 @@ export const ThemeToggleButton = () => {
   if (!isMounted) {
     return null
   }
-
-  return (
-    <SwitchCase
-      value={theme}
-      defaultComponent={
-        <motion.button
-          key="dark"
-          onClick={handleToggle}
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-        >
-          <Lightmode />
-        </motion.button>
-      }
-      caseBy={{
-        light: (
-          <motion.button
-            key="light"
-            onClick={handleToggle}
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-          >
-            <Darkmode />
-          </motion.button>
-        ),
-        dark: (
-          <motion.button
-            key="dark"
-            onClick={handleToggle}
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-          >
-            <Lightmode />
-          </motion.button>
-        ),
-      }}
-    />
-  )
+  if (theme === "light") {
+    return (
+      <motion.button
+        key="dark"
+        onClick={handleToggle}
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+      >
+        <Lightmode />
+      </motion.button>
+    )
+  }
+  if (theme === "dark") {
+    return (
+      <motion.button
+        key="dark"
+        onClick={handleToggle}
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+      >
+        <Lightmode />
+      </motion.button>
+    )
+  }
 }
