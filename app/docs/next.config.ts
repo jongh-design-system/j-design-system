ff66d9fb35f9889d612428953f280fbe857dcdb5
whdgur5717 <whdgur57117@gmail.com>
2025-03-17 21:15:20 +0900

    feat(docs): remove output and trailingSlash options from next.config.ts

--- app/docs/next.config.ts
+++ app/docs/next.config.ts
@@ -2,8 +2,6 @@ import createMDX from "@next/mdx"
 import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  trailingSlash: true,
   pageExtensions: ["js", "md", "mdx", "ts", "tsx"],
 }
 
