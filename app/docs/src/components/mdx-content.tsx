1013bed6b77cca1574d62ece0072d46fda5b9735
이종혁 <120625398+whdgur5717@users.noreply.github.com>
2025-03-23 20:51:20 +0900

    velite를 사용한 문서화 프로세스 구축 (#153)
    * fix(theme-toggle): remove SwitchCase component
    - not rendered properly in production,
    
    * feat(docs-velite): add velite config
    
    * feat(docs-config): integrate velite config in next
    - change extension ts -> mjs due to esm/require issue
    
    * feat(velite): add MDXContent component for rendering MDX code using velite
    
    * feat(docs): add Button component documentation and build with velite
    
    * feat(docs): add dynamic page generation for MDX content based on slug
    
    * chore(docs): add path mapping for velite content in tsconfig
    
    * feat(icons): check,copy icon
    
    * feat(docs): add CodeBlock component with copy button
    
    * feat(docs): enhance MDX component integration with custom component
    - using previous mdx-components
    
    * feat(velite): update config
    - using rehype plugin(shiki)
    - add draft option in schema for check prepared or not
    
    * feat(docs): add styles for Shiki code highlighting
    
    * feat(docs-button): update Button documentation
    
    * feat(docs): update dependencies
    - remove unused dependencies(next/mdx,rehype)
    - add velite, shikijs/rehype
    
    * fix(codeBlock): correct copy button icon logic
    
    * refactor(docs): rename useMDXComponents to MDXComponents for clarity
    
    ---------
    
    Co-authored-by: whdgur5717 <whdgur57117@gmail.com>

---
+++ app/docs/src/components/mdx-content.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as runtime from "react/jsx-runtime"
+
+import { MDXComponents } from "@/mdx-components"
+
+import { Button } from "./button"
+import { CodeBlock } from "./codeBlock"
+import { CommandCodeBlock } from "./commandCodeBlock"
+
+const sharedComponents = {
+  // Add your global components here
+  Button,
+  CommandCodeBlock,
+  ...MDXComponents(),
+  pre: (props: any) => <CodeBlock {...props} />,
+}
+
+// parse the Velite generated MDX code into a React component function
+const useMDXComponent = (code: string) => {
+  const fn = new Function(code)
+  return fn({ ...runtime }).default
+}
+
+interface MDXProps {
+  code: string
+  components?: Record<string, React.ComponentType>
+}
+
+// MDXContent component
+export const MDXContent = ({ code, components }: MDXProps) => {
+  const Component = useMDXComponent(code)
+  return <Component components={{ ...sharedComponents, ...components }} />
+}
--- app/docs/src/components/theme/themeToggleButton.tsx
+++ app/docs/src/components/theme/themeToggleButton.tsx
@@ -6,8 +6,6 @@ import Darkmode from "@/components/icons/darkmode"
 import Lightmode from "@/components/icons/lightmode"
 import { useIsMounted } from "@/hooks/useIsMounted"
 
-import { SwitchCase } from "../switchCase"
-
 export const ThemeToggleButton = () => {
   const isMounted = useIsMounted()
   const { theme, setTheme } = useTheme()
@@ -20,31 +18,23 @@ export const ThemeToggleButton = () => {
     return null
   }
 
-  return (
-    <SwitchCase
-      value={theme}
-      caseBy={{
-        light: (
-          <motion.button
-            key="light"
-            onClick={handleToggle}
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-          >
-            <Darkmode />
-          </motion.button>
-        ),
-        dark: (
-          <motion.button
-            key="dark"
-            onClick={handleToggle}
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-          >
-            <Lightmode />
-          </motion.button>
-        ),
-      }}
-    />
+  return theme === "light" ? (
+    <motion.button
+      key="dark"
+      onClick={handleToggle}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+    >
+      <Darkmode />
+    </motion.button>
+  ) : (
+    <motion.button
+      key="light"
+      onClick={handleToggle}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+    >
+      <Lightmode />
+    </motion.button>
   )
 }
