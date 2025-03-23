69d6c19a3553afc49cb3f50dbd420d98f56f0759
whdgur5717 <whdgur57117@gmail.com>
2025-03-23 23:11:13 +0900

    refactor(docs): update folder structure - docs/* and docs/components/*

--- app/docs/src/app/docs/[slug]/page.tsx
+++ app/docs/src/app/docs/[slug]/page.tsx
@@ -3,18 +3,18 @@ import { notFound } from "next/navigation"
 import path from "path"
 
 import { MDXContent } from "@/components/mdx-content"
-import { components } from "#site/content"
+import { intro } from "#site/content"
 
 type Props = {
   params: Promise<{ slug: string }>
 }
 
 function getPageBySlug(slug: string) {
-  return components.find((post) => post.slug === slug)
+  return intro.find((post) => post.slug === slug)
 }
 
 export async function generateStaticParams() {
-  const contentPath = path.join(process.cwd(), "content/docs/components")
+  const contentPath = path.join(process.cwd(), "content/docs/intro")
   const files = fs.readdirSync(contentPath)
   const jsonFiles = files.filter((file) => file.endsWith(".mdx"))
 
@@ -26,6 +26,7 @@ export async function generateStaticParams() {
 export default async function Page({ params }: Props) {
   const { slug } = await params
   const post = getPageBySlug(slug)
+
   if (!post) {
     return notFound()
   }
