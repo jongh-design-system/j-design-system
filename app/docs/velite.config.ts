1592e5390db4f9c2b0df7665aa195df3aec9f175
whdgur5717 <whdgur57117@gmail.com>
2025-03-23 23:11:55 +0900

    feat(velite): define new collections for changed route

--- app/docs/velite.config.ts
+++ app/docs/velite.config.ts
@@ -1,26 +1,38 @@
 import rehypeShiki from "@shikijs/rehype"
-import { defineConfig, s } from "velite"
+import { defineCollection, defineConfig, s } from "velite"
 
 // `s` is extended from Zod with some custom schemas,
 // you can also import re-exported `z` from `velite` if you don't need these extension schemas.
 
+const components = defineCollection({
+  name: "Components", // collection type name
+  pattern: "docs/components/*.mdx", // content files glob pattern
+  schema: s
+    .object({
+      title: s.string().max(99), // Zod primitive type
+      slug: s.slug("components"), // validate format, unique in posts collection
+      code: s.mdx(),
+      draft: s.boolean().optional(),
+    })
+    // more additional fields (computed fields)
+    .transform((data) => {
+      return { ...data, permalink: `/blog/${data.slug}` }
+    }),
+})
+
+const intro = defineCollection({
+  name: "Intro",
+  pattern: "docs/intro/*.mdx",
+  schema: s.object({
+    code: s.mdx(),
+    slug: s.slug("docs"),
+  }),
+})
+
 export default defineConfig({
   collections: {
-    posts: {
-      name: "Docs", // collection type name
-      pattern: "docs/*.mdx", // content files glob pattern
-      schema: s
-        .object({
-          title: s.string().max(99), // Zod primitive type
-          slug: s.slug("posts"), // validate format, unique in posts collection
-          code: s.mdx(),
-          draft: s.boolean().optional(),
-        })
-        // more additional fields (computed fields)
-        .transform((data) => {
-          return { ...data, permalink: `/blog/${data.slug}` }
-        }),
-    },
+    components,
+    intro,
   },
   mdx: {
     rehypePlugins: [
@@ -31,11 +43,5 @@ export default defineConfig({
         },
       ],
     ],
-  },
-  prepare: (data) => {
-    if (process.env.NODE_ENV === "development") {
-      return
-    }
-    data.posts = data.posts.filter((post) => post.draft === true)
   },
 })
