c5b90ebc82ff3d01d63c7804d978a5b3391b9a7b
whdgur5717 <whdgur57117@gmail.com>
2025-03-23 20:07:03 +0900

    feat(docs): add CodeBlock component with copy button

---
+++ app/docs/src/components/codeBlock/index.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { css } from "@styled-system/css"
+import { type ComponentProps, useRef, useState } from "react"
+
+import Check from "@/components/icons/check"
+import Copy from "@/components/icons/copy"
+
+export const CodeBlock = ({
+  className = "",
+  children,
+  ...props
+}: ComponentProps<"pre">) => {
+  const [isCopied, setIsCopied] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const preRef = useRef<HTMLPreElement>(null)
+
+  // 코드 복사 버튼 이벤트 핸들러
+  const handleClickCopy = async () => {
+    const code = preRef.current?.textContent // 코드 블록 내용 추출
+
+    if (code) {
+      setIsLoading(true)
+      await navigator.clipboard.writeText(code) // 클립보드에 복사
+      setIsLoading(false)
+      setIsCopied(true)
+
+      setTimeout(() => {
+        setIsCopied(false)
+      }, 1500)
+    }
+  }
+
+  return (
+    <div
+      className={css({
+        position: "relative",
+      })}
+    >
+      <button
+        disabled={isCopied || isLoading}
+        aria-label={isCopied ? "Copied!" : "Copy code"}
+        onClick={handleClickCopy}
+        className={css({
+          position: "absolute",
+          right: "1",
+          top: "1",
+          display: "flex",
+          h: "fit-content",
+          w: "fit-content",
+          alignItems: "center",
+          borderRadius: "sm",
+          px: "2",
+          py: "2",
+          color: "white",
+          cursor: "pointer",
+          zIndex: 10,
+        })}
+      >
+        {!isCopied ? <Check style={{ color: "white" }} /> : <Copy />}
+      </button>
+      <pre ref={preRef} {...props} className={className}>
+        {children}
+      </pre>
+    </div>
+  )
+}
+
+//pre 이후 스타일은 globals.css에서 담당
