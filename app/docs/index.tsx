ebc936768bbb1a696c5d9e9d664677abada25b59
whdgur5717 <whdgur57117@gmail.com>
2025-06-03 22:31:11 +0900

    feat(docs): add animateText

---
+++ app/docs/index.tsx
@@ -0,0 +1,206 @@
+import { AnimatePresence, motion, type Variants } from "motion/react"
+import {
+  Children,
+  type ElementType,
+  isValidElement,
+  memo,
+  type PropsWithChildren,
+  type ReactNode,
+} from "react"
+
+type SplitBy = "words" | "chars" | "lines"
+
+type AnimationType = "fadeUp" | "fadeDown" | "fadeLeft" | "fadeRight"
+
+interface AnimateTextProps {
+  children?: ReactNode
+  by?: SplitBy
+  as?: ElementType
+  animation?: AnimationType
+  staggerDelay?: number
+  itemDelay?: number
+  duration?: number
+  className?: string
+}
+
+const animationVariants = {
+  fadeUp: {
+    hidden: {
+      opacity: 0,
+      y: 30,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+  fadeDown: {
+    hidden: {
+      opacity: 0,
+      y: -30,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+  fadeLeft: {
+    hidden: {
+      opacity: 0,
+      x: 30,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+  fadeRight: {
+    hidden: {
+      opacity: 0,
+      x: -30,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+} as const
+
+const createContainerVariants = (staggerDelay: number, itemDelay: number) => ({
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: staggerDelay,
+      delayChildren: itemDelay,
+    },
+  },
+  exit: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: 1,
+    },
+  },
+})
+
+function extractTextFromChildren(children: ReactNode) {
+  if (typeof children === "string") {
+    return children
+  } else {
+    if (isValidElement<PropsWithChildren>(children)) {
+      return children.props.children as string
+    }
+  }
+}
+
+export function AnimateTextBase({
+  children,
+  by = "lines",
+  as = "div",
+  animation = "fadeUp",
+  staggerDelay = 0.1,
+  itemDelay = 0,
+  duration = 0.6,
+  className,
+}: AnimateTextProps) {
+  const MotionContainer = motion.create(as)
+
+  const itemVariants: Variants = {
+    hidden: animationVariants[animation].hidden,
+    show: {
+      ...animationVariants[animation].show,
+      transition: {
+        ...animationVariants[animation].show.transition,
+        duration,
+      },
+    },
+  }
+
+  const containerVariants = createContainerVariants(staggerDelay, itemDelay)
+
+  const renderLineItems = () => {
+    const nodes = Children.toArray(children)
+    return nodes.map((nodeContent, index) => {
+      const key = `line-item-${index}`
+      if (!isValidElement(nodeContent)) {
+        return null
+      }
+      return (
+        <motion.div
+          key={key}
+          variants={itemVariants}
+          {...(nodeContent.props || {})}
+        />
+      )
+    })
+  }
+
+  const renderItems = (by: SplitBy) => {
+    const segment = extractTextFromChildren(children)
+    if (typeof segment !== "string" || !segment) {
+      return null
+    }
+    if (by === "chars") {
+      const nodes = segment.split("")
+
+      return nodes?.map((nodeContent, index) => {
+        const key = `segment-char-${index}`
+
+        const MotionComponent = motion.create("span")
+
+        return (
+          <MotionComponent key={key} variants={itemVariants}>
+            {nodeContent}
+          </MotionComponent>
+        )
+      })
+    }
+
+    if (by === "words") {
+      const nodes = segment.split(/\s+/)
+
+      return nodes?.map((nodeContent, index) => {
+        const key = `segment-word-${index}`
+
+        const MotionComponent = motion.create("span")
+
+        return (
+          <MotionComponent key={key} variants={itemVariants}>
+            {nodeContent}
+          </MotionComponent>
+        )
+      })
+    }
+  }
+
+  return (
+    <AnimatePresence mode="popLayout">
+      <MotionContainer
+        variants={containerVariants}
+        initial="hidden"
+        animate="show"
+        exit="exit"
+        className={className}
+      >
+        {by === "lines" ? renderLineItems() : renderItems(by)}
+      </MotionContainer>
+    </AnimatePresence>
+  )
+}
+
+export const AnimateText = memo(AnimateTextBase)
