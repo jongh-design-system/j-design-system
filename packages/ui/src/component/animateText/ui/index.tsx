c1fe0de0e61b393f6ae150b2d74a6dc83f580523
이종혁 <120625398+whdgur5717@users.noreply.github.com>
2025-06-06 17:07:04 +0900

    AnimateText 컴포넌트 구현 (#167)
    * chore(*): fix typo in prettierignore
    
    * feat(ui/animateText): add AnimateText component for animated text display
    
    * feat(ui/animateText): enhance AnimateText
    - various animation options
    - enforce children type based on splitBy
    
    * chore(ui/animateText): improve readability
    
    * feat(ui/animateText): add className prop
    
    * feat(docs): add AnimateText component
    
    * feat(docs/main): change mainPage style
    
    * feat(ui/animateText): export memoized version
    
    * feat(ui/animateText): refine children type validation in extractTextFromChildren
    
    * feat(docs): add animateText
    
    * feat(ui/animateText): whitespace handling
    
    * feat(stories): add AnimateText component stories
    
    * feat(docs/main): mainPage layout style
    
    * chore(ui/animateText): change animation defaultValue
    
    * feat(ui/animateText): eliminate exit animation
    
    * feat(ui/animateText): add inline-block style to words
    
    * feat(ui/animateText): extend props for customization
    - extend MotionProps and customize initial,animate prop
    
    * feat(ui/animateText): add stories about motion wrapper
    
    ---------
    
    Co-authored-by: whdgur5717 <whdgur57117@gmail.com>

---
+++ packages/ui/src/component/animateText/ui/index.tsx
@@ -0,0 +1,229 @@
+import { motion, type MotionProps, type Variants } from "framer-motion"
+import {
+  Children,
+  type ElementType,
+  isValidElement,
+  memo,
+  type PropsWithChildren,
+  type ReactNode,
+} from "react"
+
+type SplitBy = "words" | "chars" | "lines"
+
+type AnimationType = "fadeUp" | "fadeDown" | "fadeLeft" | "fadeRight"
+
+interface AnimateTextProps extends MotionProps {
+  children?: ReactNode
+  by?: SplitBy
+  as?: ElementType
+  animation?: AnimationType
+  staggerDelay?: number
+  itemDelay?: number
+  duration?: number
+  className?: string
+}
+
+const animationVariants = {
+  fadeUp: {
+    hidden: {
+      opacity: 0,
+      y: 30,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+  fadeDown: {
+    hidden: {
+      opacity: 0,
+      y: -30,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+  fadeLeft: {
+    hidden: {
+      opacity: 0,
+      x: 30,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+  fadeRight: {
+    hidden: {
+      opacity: 0,
+      x: -30,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  },
+} as const
+
+const createContainerVariants = (staggerDelay: number, itemDelay: number) => ({
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: staggerDelay,
+      delayChildren: itemDelay,
+    },
+  },
+})
+
+function extractTextFromChildren(children: ReactNode) {
+  if (typeof children === "string") {
+    return children
+  } else {
+    if (isValidElement<PropsWithChildren>(children)) {
+      return children.props.children as string
+    }
+  }
+}
+
+export function AnimateTextBase({
+  children,
+  by = "lines",
+  as = "div",
+  animation = "fadeLeft",
+  staggerDelay = 0.05,
+  itemDelay = 0,
+  duration = 0.4,
+  className,
+  initial = "hidden",
+  animate = "show",
+  ...rest
+}: AnimateTextProps) {
+  const MotionContainer = motion.create(as)
+
+  const selectedAnimationVariant = animationVariants[animation]
+
+  const itemVariants: Variants = {
+    hidden: selectedAnimationVariant.hidden,
+    show: {
+      ...selectedAnimationVariant.show,
+      transition: {
+        ...selectedAnimationVariant.show.transition,
+        duration,
+      },
+    },
+  }
+
+  const containerVariants = createContainerVariants(staggerDelay, itemDelay)
+
+  const renderLineItems = () => {
+    const nodes = Children.toArray(children)
+    return nodes.map((nodeContent, index) => {
+      const key = `line-item-${index}`
+      if (!isValidElement(nodeContent)) {
+        return null
+      } //for type safety
+      return (
+        <motion.div
+          key={key}
+          variants={itemVariants}
+          {...(nodeContent.props || {})}
+        />
+      )
+    })
+  }
+
+  const renderItems = (by: SplitBy) => {
+    const segment = extractTextFromChildren(children)
+    if (typeof segment !== "string" || !segment) {
+      return null
+    }
+    if (by === "chars") {
+      const nodes = segment.split("")
+
+      return nodes?.map((nodeContent, index) => {
+        const key = `segment-char-${index}`
+
+        const MotionComponent = motion.create("span")
+
+        return (
+          <MotionComponent key={key} variants={itemVariants}>
+            {nodeContent}
+          </MotionComponent>
+        )
+      })
+    }
+
+    if (by === "words") {
+      const nodes = segment.split(/(\s+)/)
+
+      return nodes?.map((nodeContent, index) => {
+        const key = `segment-word-${index}`
+        const MotionComponent = motion.create("span")
+
+        if (nodeContent.trim() === "") {
+          return <span key={key}>{nodeContent}</span>
+        }
+
+        return (
+          <MotionComponent
+            key={key}
+            variants={itemVariants}
+            style={{
+              display: "inline-block",
+            }}
+          >
+            {nodeContent}
+          </MotionComponent>
+        )
+      })
+    }
+  }
+
+  return (
+    <MotionContainer
+      variants={containerVariants}
+      initial={initial}
+      animate={animate}
+      className={className}
+      {...rest}
+    >
+      {by === "lines" ? renderLineItems() : renderItems(by)}
+    </MotionContainer>
+  )
+}
+/**
+ * Children constraints depend on the 'by' prop value.
+ *
+ * ```tsx
+ * // by="lines": Array of React elements
+ * <AnimateText by="lines">
+ * <div>First line</div>
+ * <div>Second line</div>
+ * </AnimateText>
+ *
+ * // by="words" | "chars": String or single element containing string
+ * <AnimateText by="words">Split by words</AnimateText>
+ * <AnimateText by="chars"><p>Split by characters</p></AnimateText>
+ * ```
+ */
+export const AnimateText = memo(AnimateTextBase)
