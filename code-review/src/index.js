85da31b7385570ba2e143ff93ba1367888864110
whdgur5717 <whdgur5717@gmail.com>
2026-06-23 23:20:04 +0900

    feat(code-review): build codex review packets

--- code-review/src/index.js
+++ code-review/src/index.js
@@ -5,4 +5,5 @@ export {
 export { readCommitsFromLocalGit } from "./git-commit-reader.js";
 export { readFileDiffsFromLocalGit } from "./git-diff-reader.js";
 export { filterReviewTargets } from "./review-targets.js";
+export { buildReviewPackets, formatReconcilePrompt, formatReviewPrompt } from "./review-packets.js";
 export { parseUnifiedDiffByFile } from "./unified-diff-parser.js";
