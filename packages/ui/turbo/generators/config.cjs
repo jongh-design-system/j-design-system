60f4508ea15dcba656ae304ae63234ce6e2f7c7a
whdgur5717 <whdgur5717@gmail.com>
2026-08-24 20:36:55 +0900

    chore: remove unused tooling packages

--- packages/ui/turbo/generators/config.cjs
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const storyGen = require("./stories.cjs")
-const uiGen = require("./ui.cjs")
-module.exports = function generator(plop) {
-  storyGen(plop)
-  uiGen(plop)
-}
